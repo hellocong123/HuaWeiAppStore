753547bfdebc2b29b9a6b24cd7d860064d363ce9
helloworldcong <502088663@qq.com>
2018-01-17 17:36:43 +0800

    测试，添加一个Log

--- app/src/main/java/com/cong/huaweiappstore/mvp/view/RecommendFragmentPresenterImpl.java
+++ app/src/main/java/com/cong/huaweiappstore/mvp/view/RecommendFragmentPresenterImpl.java
@@ -1,11 +1,14 @@
 package com.cong.huaweiappstore.mvp.view;
 
 import android.os.SystemClock;
+import android.util.Log;
 
 import com.cong.huaweiappstore.mvp.BasePresenterImpl;
 import com.cong.huaweiappstore.mvp.persenter.RecommendFragmentPresenter;
 
 import javax.inject.Inject;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Administrator on 2018/1/16
@@ -25,6 +28,7 @@ public class RecommendFragmentPresenterImpl extends BasePresenterImpl<RecommendF
             public void run() {
                 SystemClock.sleep(2000);
                 presenterView.onRecommendDataError();
+                Log.i(TAG, "run: 推荐");
             }
         }).start();
 
